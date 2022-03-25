fee6b611c0691b96d18862b9ff14ba8e4d6c9828
Vladislav Russo <vladtirma@gmail.com>
2022-03-25 23:46:39 +0300

    L2

--- Assets/CustomEditorTool.cs
+++ Assets/CustomEditorTool.cs
@@ -5,7 +5,7 @@ using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.UI;
 
-[EditorTool(displayName:"PointConection",typeof(ShapPoint))]
+[EditorTool(displayName:"PointConection",typeof(AuxiliaryScript))]
 public class CustomEditorTool : EditorTool
 {
     [SerializeField] private Texture2D toolIcon;
@@ -23,17 +23,42 @@ public class CustomEditorTool : EditorTool
     }
     public override void OnToolGUI(EditorWindow window)
     {
-        Transform targetCustomSnep = ((ShapPoint)target).transform;
+        Transform targetCustomSnep = ((AuxiliaryScript)target).transform;
         EditorGUI.BeginChangeCheck();
         Vector3 newPos = Handles.PositionHandle(targetCustomSnep.transform.position, Quaternion.identity);
         if(EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(targetCustomSnep,name = "Lol");
-            targetCustomSnep.transform.position = newPos;
+            ClickNullPosition(targetCustomSnep,newPos); 
         }
     }
     private void ClickNullPosition(Transform targetCustomSnep, Vector3 newPos)
     {
-        
+        CoustomPoint[] allPoint = FindObjectsOfType<CoustomPoint>();
+        CoustomPoint[] targetPoints = targetCustomSnep.GetComponents<CoustomPoint>();
+        Vector3 bestposition = newPos;
+        float closesDistance = float.PositiveInfinity;
+        foreach(CoustomPoint point in allPoint)
+        {
+            if (point.transform.parent == targetCustomSnep) continue;
+            foreach(CoustomPoint ownPoint in targetPoints)
+            {
+                Vector3 targetPos = point.transform.position - (ownPoint.transform.position - targetCustomSnep.position);
+                float distance = Vector3.Distance(targetPos, newPos);
+                if(distance < closesDistance)
+                {
+                    closesDistance = distance;
+                    bestposition = targetPos; 
+                }
+            }
+        }
+        if(closesDistance < 0.5f)
+        {
+            targetCustomSnep.position = bestposition;
+        }
+        else
+        {
+            targetCustomSnep.position = newPos;
+        }
     }
 }
